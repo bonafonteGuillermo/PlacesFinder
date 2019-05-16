d9c6a1ddc0064a6020fc68cce29dbe320711dfe9
Guille <gbcriado@gmail.com>
2019-05-16 20:58:58 +0200

    Added some colors and loading in splash

--- app/src/main/java/com/upsa/mimo/placesfinder/splash/SplashFragment.kt
+++ app/src/main/java/com/upsa/mimo/placesfinder/splash/SplashFragment.kt
@@ -3,10 +3,12 @@ package com.upsa.mimo.placesfinder.splash
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.upsa.mimo.placesfinder.R
+import kotlinx.android.synthetic.main.fragment_splash.*
 
 class SplashFragment: Fragment() {
 
@@ -15,8 +17,12 @@ class SplashFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         inflater.inflate(R.layout.fragment_splash, container, false)
 
-    override fun onStart() {
-        super.onStart()
-        Handler().postDelayed({ router.navigateToBaseActivity() }, 2000)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({ router.navigateToBaseActivity() }, 5000)
+        loading.animate().apply {
+            duration = 500
+        }
     }
+
 }
