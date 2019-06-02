a9a60c5ba8455d0bf636092e6af7f66b2d3cfc5c
Guillermo Bonafonte Criado <guillermo.bonafonte@ing.com>
2019-06-02 16:39:55 +0200

    Added menu

--- app/src/main/java/com/upsa/mimo/placesfinder/app/BaseActivity.kt
+++ app/src/main/java/com/upsa/mimo/placesfinder/app/BaseActivity.kt
@@ -1,13 +1,16 @@
 package com.upsa.mimo.placesfinder.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.upsa.mimo.placesfinder.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 /**
  *
@@ -22,11 +25,18 @@ class BaseActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bottom_nav_view.setupWithNavController(navController)
+        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
-
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { item.itemId
+        when(item.itemId){
+            R.id.action_settings -> Log.d("->", "Settings")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
